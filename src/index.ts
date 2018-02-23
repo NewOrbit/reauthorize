fa271231872b35a652ac717ff36d11a2469a694f
Henry Ing-Simmons <hing-simmons@neworbit.co.uk>
2018-02-23 15:58:13 +0000

    Export Authorise component and authorise hoc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import Authorise from "./Authorise";
+
 export { AuthorisationResult, AuthorisationSetting, AuthState, User } from "./model";
 export { configureAuthMiddleware, AuthMiddlewareOptions, RouteResult } from "./authMiddleware";
 export { isAuthorised } from "./isAuthorised";
+export { authorise } from "./authoriseHoc";
+
+export {
+    Authorise
+};
